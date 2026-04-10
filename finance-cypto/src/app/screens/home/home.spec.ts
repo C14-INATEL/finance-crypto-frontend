44ca170e5566a67df03a57fdc7744e9f8f53f7a7
João Pedro <mareirajoaopedro995@gmail.com>
2026-04-09 22:40:49 -0300

    test: implementa funcoes front-end e testes unitarios na Home

--- finance-cypto/src/app/screens/home/home.spec.ts
+++ finance-cypto/src/app/screens/home/home.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing'; 
 import { Home } from './home';
 
 describe('Home', () => {
@@ -8,7 +8,7 @@ describe('Home', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Home],
+      imports: [Home, RouterTestingModule], 
     }).compileComponents();
 
     fixture = TestBed.createComponent(Home);
@@ -19,4 +19,47 @@ describe('Home', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // --- TESTES UNITÁRIOS  ---
+
+  it('Teste 1: Deve alternar a visibilidade do saldo na tela', () => {
+    expect(component.saldoVisivel).toBe(true); 
+    
+    component.alternarVisibilidadeSaldo(); 
+    expect(component.saldoVisivel).toBe(false); 
+    
+    component.alternarVisibilidadeSaldo(); 
+    expect(component.saldoVisivel).toBe(true); 
+  });
+
+  it('Teste 2: Deve aplicar a classe CSS correta na badge de rentabilidade', () => {
+    expect(component.obterClasseBadge(5.2)).toBe('positive');
+    expect(component.obterClasseBadge(-1.2)).toBe('negative');
+  });
+
+  it('Teste 3: Deve aumentar o patrimônio ao comprar um ativo válido', () => {
+    component.patrimonioTotal = 1000; 
+    
+    const compraRealizada = component.comprarAtivo(500); 
+    
+    expect(compraRealizada).toBe(true);
+    expect(component.patrimonioTotal).toBe(1500); 
+    
+    expect(component.comprarAtivo(-100)).toBe(false); 
+  });
+
+  it('Teste 4: Deve diminuir o patrimônio ao vender ativo e impedir saldo negativo', () => {
+    component.patrimonioTotal = 2000; 
+    
+    const vendaRealizada = component.venderAtivo(500); 
+    
+    expect(vendaRealizada).toBe(true);
+    expect(component.patrimonioTotal).toBe(1500); 
+    
+    const vendaInvalida = component.venderAtivo(5000); 
+    
+    expect(vendaInvalida).toBe(false); 
+    expect(component.patrimonioTotal).toBe(1500); 
+  });
+
 });
--- finance-cypto/src/app/screens/home/home.ts
+++ finance-cypto/src/app/screens/home/home.ts
@@ -1,9 +1,43 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.html',
-  styleUrl: './home.scss',
+  styleUrls: ['./home.scss']
 })
-export class Home {}
+export class Home {
+
+  patrimonioTotal: number = 14520; //valor fixo para simular o patrimônio total do usuário, final sera alterado para ser dinamico, puxando do backend
+  
+
+  saldoVisivel: boolean = true;
+
+  alternarVisibilidadeSaldo(): void {
+    this.saldoVisivel = !this.saldoVisivel;
+  }
+
+
+  obterClasseBadge(porcentagem: number): string {
+    return porcentagem >= 0 ? 'positive' : 'negative';
+  }
+
+
+  comprarAtivo(valor: number): boolean {
+    if (valor <= 0) return false; 
+    this.patrimonioTotal += valor;
+    return true;
+  }
+
+
+  venderAtivo(valor: number): boolean {
+    if (valor > this.patrimonioTotal || valor <= 0) {
+      return false; 
+    }
+    this.patrimonioTotal -= valor;
+    return true;
+  }
+}
